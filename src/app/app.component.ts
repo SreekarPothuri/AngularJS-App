92a89acbe76bc9a2582075af3778e652a391506d
Your Name <you@example.com>
2020-12-26 01:04:38 +0530

    Added: code to validate and show error message incase of Incorrect input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl="../assets/fav.jpg";
   url="https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello From BridgeLabz"
@@ -18,5 +19,15 @@ export class AppComponent {
   onClick($event){
     console.log("Save button is clicked!!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event){
+    console.log("Change Event Occured!!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
   }
 }
