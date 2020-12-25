1ee846189ca84f58c060b1e028cfc0caedb231b7
Your Name <you@example.com>
2020-12-26 00:17:27 +0530

    Added: code to display hello message using One Way Data Binding technique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello From BridgeLabz';
+  title = 'HelloWorldAJS';
+
+  ngOnInit(): void {
+    this.title = "Hello From BridgeLabz"
+  }
 }
